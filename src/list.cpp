635643c1a6c819a1460ed3743870a1f9483faf53
NumMeril <misa.merkulov78@gmail.com>
2023-11-06 23:06:11 +0300

    added listVerify listRealloc

--- src/list.cpp
+++ src/list.cpp
@@ -2,9 +2,13 @@
 
 
 int listInsert( List* lst, int index, int value ){
+    #ifndef _NO_LIST_VERIFY
+        int res = listVerify( lst );
+        if ( res ) return res;
+    #endif
 
     // if prev[index] is -1, then this index isn't used in List, so return error code
-    if ( lst->prev[index] <= -1 ) return -1;
+    if ( lst->prev[index] <= -1 ) return FICTIVE_INDEX;
 
     /*
         listInsert( &lst, 1, 2 )
@@ -53,11 +57,16 @@ int listInsert( List* lst, int index, int value ){
 
 int listDelete( List* lst, int index ){
 
+    #ifndef _NO_LIST_VERIFY
+        int res = listVerify( lst );
+        if ( res ) return res;
+    #endif
+
     // if prev[index] is -1, then this index isn't used in List, so return error code
-    if ( lst->prev[index] == -1 ) return -1;
+    if ( lst->prev[index] == -1 ) return FICTIVE_INDEX;
 
     // we CAN NOT delete 0 index
-    if ( index == 0 )             return -1;
+    if ( index == 0 )             return ZERO_DELETE;
 
     /*
         listDelete( &lst, 2 )
@@ -82,8 +91,10 @@ int listDelete( List* lst, int index ){
 
     int prev = lst->prev[index], \
         next = lst->next[index];
+
     // next(1): 3
     lst->next[prev] = next;
+
     // prev(3): 1
     lst->prev[next] = prev;
 
@@ -92,9 +103,11 @@ int listDelete( List* lst, int index ){
     lst->data[index] = -1;
     lst->prev[index] = -1;
 
+    // updates singly linked list of fictive elements
     lst->next[index] = lst->free;
     lst->free = index;
 
+    // if element's previous index is 0, then this element is head
     if ( lst->prev[prev] == 0 ) lst->head = prev;
     if ( lst->prev[next] == 0 ) lst->head = prev;
 
@@ -105,18 +118,18 @@ int listDelete( List* lst, int index ){
 
 
 
-int listCTOR( List* lst ){
+int listCTOR( List* lst, int size = LIST_SIZE ){
 
     lst->free = 1;
     lst->head = 0;
     lst->tail = 0;
     lst->size = 0;
-    lst->capt = LIST_SIZE;
+    lst->capt = size;
 
-    lst->next = ( int* )  calloc( sizeof( int ),  LIST_SIZE );
-    lst->prev = ( int* )  calloc( sizeof( int ),  LIST_SIZE );
+    lst->next = ( int* )  calloc( sizeof( int ),  lst->capt );
+    lst->prev = ( int* )  calloc( sizeof( int ),  lst->capt );
 
-    for ( int i = 0; i < LIST_SIZE; i++ ){
+    for ( int i = 0; i < lst->capt; i++ ){
         lst->next[i] = i + 1;
         lst->prev[i] = -1;
     }
@@ -124,9 +137,9 @@ int listCTOR( List* lst ){
     lst->prev[0] = 0;
     lst->next[0] = 0;
     // next(last_index) = 0
-    lst->next[LIST_SIZE - 1] = 0;
+    lst->next[lst->capt - 1] = 0;
 
-    lst->data = ( elem* ) calloc( sizeof( elem ), LIST_SIZE );
+    lst->data = ( elem* ) calloc( sizeof( elem ), lst->capt );
 
     // if one of the callocs didn't work, we return an error code
     if ( !lst->next ||
@@ -190,4 +203,56 @@ void listDump( List* lst ){
         printf("%5d", lst->prev[i]);
     }
     printf("\n--END OF LIST--\n\n");
+}
+
+int listVerify( List* lst ){
+
+    if ( !lst )
+        return LIST_NULL;
+
+    if ( lst->prev[0] == -1 )
+        return ZERO_IS_FICTIVE;
+
+    #ifdef _AUTO_LIST_REALLOC
+        if ( lst->capt == lst.size + 1 )
+            listRealloc( lst );
+    #endif
+
+    return 0;
+}
+
+
+int listRealloc( List* lst, int size = 0 ){
+
+    List* tmp = {};
+    int res = 0
+
+    if ( !size )
+        res = listCTOR( tmp, lst->capt * _LIST_MULTIPLIER );
+    else
+        res = listCTOR( tmp, size );
+
+    if ( res )
+        return res
+
+    lst->free = tmp->free;
+    lst->head = tmp->head;
+    lst->tail = tmp->tail;
+
+    for ( int i = 0; i < tmp->capt; i++ ){
+        if ( i == lst->capt )
+            break;
+
+        tmp->prev[i] = lst->prev[i];
+        tmp->next[i] = lst->next[i];
+        tmp->data[i] = lst->data[i];
+    }
+
+    tmp->size = lst->capt < tmp->capt ? lst->capt : tmp->capt;
+
+    listDTOR( lst );
+
+    lst = tmp;
+
+    return res;
 }
--- src/list.h
+++ src/list.h
@@ -6,6 +6,7 @@ typedef int elem;
 
 
 const int LIST_SIZE = 11;
+const int _LIST_MULTIPLIER = 2;
 
 
 struct List{
@@ -28,7 +29,7 @@ struct List{
     \param lst - pointer to List struct
     \return 1 if failed to allocate memory, 0 otherwise
 */
-int listCTOR( List* lst );
+int listCTOR( List* lst, int size = LIST_SIZE )
 
 /*
     \brief destroys struct List
@@ -40,12 +41,64 @@ void listDTOR( List* lst );
 /*
     \brief adds new element to List
     Inserts new element right after [index] element. If there is no data in [index], then returns error code.
+
     Updates tail if new element added right after previous tail.
 
-    \return -1 if [index] is not used in lst. Otherwise - returns index of inserted element
+    \return FICTIVE_INDEX if [index] is not used in lst.
+            error         some value from listVerify. Accures, if the list is incorrect
+            idx           otherwise returns index of inserted element.
+    FICTIVE_INDEX is negative.
 */
 int listInsert( List* lst, int index, int value );
 
+/*
+    \brief deletes element [index]
+    Deletes element [index] from list.
+    If there is no data in [index], then returns error code.
+    If index equals to 0, then returns error code (we can't delete index 0 element).
+
+    Updates head if some element now has 0 as prev index.
+
+    \return FICTIVE_INDEX if [index] is not used in lst
+            ZERO_DELETE   if [index] equals to 0
+            error         some value from listVerify. Accures, if the list is incorrect
+            value         if not FICTIVE_INDEX and ZERO_DELETE, value is deleted value that was stored under [index]
+    ZERO_DELETE and FICTIVE_INDEX are negative
+*/
 int listDelete( List* lst, int index );
 
+/*
+    \brief reallocates list
+    Multiply current capacity by _LIST_MULTIPLIER. Exact capacity can be passed as second argument.
+
+    !!IF NEW CAPACITY IS LESS THAN CURRENT, BE READY FOR UB!!
+
+    By default, auto realloc is not active, however it can be turned on with defining _AUTO_LIST_REALLOC
+
+    \return 1 if failed to allocate memory, 0 otherwise
+*/
+
+int listRealloc( List* lst, int size = 0 );
+
+/*
+    \brief checks list if its correct
+
+    Can be turned off with defining _NO_LIST_VERIFY
+
+    \return  0                allright
+             LIST_NULL        if lst is nullptr
+             ZERO_IS_FICTIVE  if element with index 0 is fictive
+
+*/
+
+int listVerify( List* lst );
+
 void listDump( List* lst );
+
+
+enum ERRORS{
+    LIST_NULL       = -4,
+    ZERO_IS_FICTIVE = -3,
+    ZERO_DELETE     = -2,
+    FICTIVE_INDEX   = -1  // if index does not appear in list
+};
